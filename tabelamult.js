9eda2ef2b8a03e4882d55ec6956b63d04ab86482
jeferson bruno <jeferson.bruno@live.com>
2021-08-12 20:52:50 -0300

    atividade realizada com extra

---
+++ tabelamult.js
@@ -0,0 +1,23 @@
+function tabelaMulti(n=10) {
+
+    let meuArray = []
+
+
+    for (let l = 0; l <= n; l++) {
+        meuArray.push([])
+
+        for (let c = 0; c <= n; c++) {
+            meuArray[l].push(l * c)
+        }
+    }
+
+   
+    console.table(meuArray)
+}
+
+
+
+
+
+
+
